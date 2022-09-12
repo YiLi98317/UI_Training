df23fb59e6e558e6b1eb233cd1fe0d294c4d20a3
Yi Li <yilee317@gmail.com>
2022-09-12 00:23:13 -0400

    middleware & joi validation

---
+++ JavaScript/assessment/validators/index.js
@@ -0,0 +1,10 @@
+//* validators/index.js
+const register = require('./register.validator')
+const login = require('./login.validator')
+const post = require('./post.validator')
+
+module.exports = {
+    register,
+    login,
+    post
+}
---
+++ JavaScript/assessment/validators/post.validator.js
@@ -0,0 +1,10 @@
+//* validators/post.validator.js
+const Joi = require('joi');
+
+const postSchema = Joi.object({
+    title: Joi.string().min(5).required(),
+    content: Joi.string().min(1).required(),
+    tags: Joi.array().items(Joi.string()).min(2).max(4).required()
+});
+
+module.exports = postSchema;
